84b842dad6a2bff624ba7bba1e56b2031a15376a
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-09-28 23:31:02 +0700

    method chaining selesai

--- method_chaining.js
+++ method_chaining.js
@@ -1,9 +1,5 @@
 function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
-  return backwards + "!!!";
+  return data.toUpperCase().split("").reverse().join("")+ "!!!";
 }
 
 console.log(shout_backwards("Hello World"));
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -22,16 +22,49 @@
 
  // Our object with the chainable methods using class in ES6
  class UserController {
-  
-   titleCaseName(str) {
-    
-    
+   constructor(){
+     this.data = usersData
+     this.firstName
+     this.lastName
+     this.email
+     this.id
+     this.nameFormat
+     this.dataFormat
+     this.display
+   }
+   findUser(value){
+     for(var i=0; i< this.data.length; i++){
+       if(value == this.data[i].email){
+         this.firstName = this.data[i].firstName
+         this.lastName = this.data[i].lastName
+         this.email = this.data[i].email
+         this.id = this.data[i].id
+       }
+     }
+     return this
+   }
+
+   formatName(){
+     this.nameFormat = `Member Name : ${this.firstName} ${this.lastName}`
+     return this
+   }
+   formatData(){
+     this.dataFormat = `ID : ${this.id} \nEmail : ${this.email}`
+     return this
+   }
+   displayUser(){
+     this.display = `${this.nameFormat}\n${this.dataFormat}`
+     return this.display
    }
  }
 
  // Driver code
  let userController = new UserController;
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+ // console.log(userController.data[2].email);
+ // console.log(usersData[0].email);
+ console.log(userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser());
+ console.log(userController.findUser("spongebob@crustycrab.com").formatName().formatData().displayUser());
+ console.log(userController.findUser("patric.star@gmail.com").formatName().formatData().displayUser());
 
  // result
  // Member name: Squidward Tentacles
