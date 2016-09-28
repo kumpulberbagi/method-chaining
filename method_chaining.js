74121bcaf6bbe2f2bcc37b838e0807b2f2f8ff7d
name": null, &   "name": null <pair+dev.wind@devbootcamp.com>
2016-09-28 09:51:44 +0700

    Template

---
+++ method_chaining.js
@@ -0,0 +1,9 @@
+function shout_backwards(data){
+  var all_caps = data.toUpperCase();
+  var splitting = all_caps.split("");
+  var reversing = splitting.reverse()
+  var backwards = reversing.join("");
+  return backwards + "!!!";
+}
+
+console.log(shout_backwards("Hello World"));
---
+++ method_chaining_from_scratch.js
@@ -0,0 +1,39 @@
+ // The data store:
+ var usersData = [{
+     firstName: "SpongeBob",
+     lastName: "SquarePants",
+     email: "spongebob@crustycrab.com",
+     id: 102
+ }, {
+     firstName: "Patrick",
+     lastName: "Star",
+     email: "patric.star@gmail.com",
+     id: 103
+ }, {
+     firstName: "Squidward",
+     lastName: "Tentacles",
+     email: "awesomesquidward@yahoo.com",
+     id: 104
+ }]
+
+ function titleCaseName(str) {
+
+ }
+
+ // Our object with the chainable methods using class in ES6
+ class UserController {
+  
+   titleCaseName(str) {
+    
+    
+   }
+ }
+
+ // Driver code
+ let userController = new UserController;
+ userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+
+ // result
+ // Member name: Squidward Tentacles
+ // ID: 104
+ // Email: awesomesquidward@yahoo.com
