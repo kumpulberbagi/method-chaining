2845a8da4e9b34406971800968344ff59c9cbc51
dharmadi93 <dharmadi93@gmail.com>
2016-10-01 22:48:06 +0700

    chaining method

--- method_chaining.js
+++ method_chaining.js
@@ -1,9 +1,11 @@
 function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
-  return backwards + "!!!";
+  // var all_caps = data.toUpperCase();
+  // var splitting = all_caps.split("");
+  // var reversing = splitting.reverse();
+  // var backwards = reversing.join("");
+  // return backwards + "!!!";
+
+  return data.toUpperCase().split("").reverse().join("") + "!!!";
 }
 
 console.log(shout_backwards("Hello World"));
