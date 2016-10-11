6fca9615f8960a0d9671968c58216fda344b1405
Ken Duigraha Putra <email": null>
2016-10-11 10:19:54 +0700

    method chaining function

--- method_chaining.js
+++ method_chaining.js
@@ -1,10 +1,76 @@
-function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
-  return backwards + "!!!";
+// function shout_backwards(data){
+//   var all_caps = data.toUpperCase();
+//   var splitting = all_caps.split("");
+//   var reversing = splitting.reverse()
+//   var backwards = reversing.join("");
+//   return backwards + "!!!";
+// }
+//
+// console.log(shout_backwards("Hello World"));
+console.log("Hello World".toUpperCase().split("").reverse().join("") + "!!!");
+
+let shout_backwards = (data) => {
+  let all_caps = () => {
+    data = data.toUpperCase()
+    let splitting = () => {
+      data = data.split("")
+
+      let reversing = () => {
+        data = data.reverse()
+
+        let backwards = () => {
+          data = data.join("")
+
+          return data + "!!!"
+        }
+
+        return {
+          backwards: backwards
+        }
+      }
+      return {
+        reversing: reversing
+      }
+    }
+    return {
+      splitting: splitting
+    }
+  }
+  return {
+    all_caps: all_caps
+  }
 }
 
-console.log(shout_backwards("Hello World"));
-console.log("Hello World".toUpperCase().split("").reverse().join("") + "!!!");
+
+console.log(shout_backwards("Hello World").all_caps().splitting().reversing().backwards());
+
+/*
+
+}
+let all_caps = () => {
+  data = data.toUpperCase()
+  return this
+}
+
+let splitting = () => {
+  data = data.split("")
+  return this
+}
+
+let reversing = () => {
+  data = data.reverse()
+  return this
+}
+
+let backwards = () => {
+  data = data.join("")
+  return this + "!!!"
+}
+
+return {
+  all_caps: all_caps,
+  splitting: splitting,
+  reversing: reversing,
+  backwards: backwards
+}
+*/
