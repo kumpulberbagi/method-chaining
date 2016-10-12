368089abf8d96a119b3ceb6562a9e77649a41578
ajilantang <ajilantang@gmail.com>
2016-10-13 00:44:04 +0700

    push tebaru

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -15,23 +15,43 @@
      email: "awesomesquidward@yahoo.com",
      id: 104
  }]
+     // Our object with the chainable methods
+  class UserController {
 
- function titleCaseName(str) {
+    findUser(email){
+      for (let i = 0; i < usersData.length; i++){
+        if( usersData[i]["email"] === email ){
+          this.index = i;
+          break;
+        } else {
 
- }
+        }
+      }
+      return this
+    }
 
- // Our object with the chainable methods using class in ES6
- class UserController {
-  
-   titleCaseName(str) {
-    
-    
-   }
- }
+    formatName(){
+      this.name = "Member name: "+ usersData[this.index]["firstName"] +" "+usersData[this.index]["lastName"]
+      return this
+    }
+
+    formatData(){
+      this.id= "ID: "+ usersData[this.index]["id"]
+      this.email= "Email: "+ usersData[this.index]["email"]
+      return this
+    }
+
+    displayUser(){
+      console.log(this.name)
+      console.log(this.id)
+      console.log(this.email)
+    }
+  };
 
  // Driver code
- let userController = new UserController;
+ let userController = new UserController()
  userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+
 
  // result
  // Member name: Squidward Tentacles
