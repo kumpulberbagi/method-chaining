757fca1bb1701fb3234b1924e87a6e0d1cd0207a
ajilantang <ajilantang@gmail.com>
2016-10-13 13:16:44 +0700

    update

--- method_chaining.js
+++ method_chaining.js
@@ -1,9 +1,21 @@
 function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
-  return backwards + "!!!";
+  return backwards(data) + "!!!";
+}
+
+var all_caps = function(data){
+  return data.toUpperCase();
+}
+
+var splitting = function (data){
+  return all_caps(data).split("");
+}
+
+var reversing = function(data){
+  return splitting(data).reverse()
+}
+
+var backwards = function (data){
+  return reversing(data).join("");
 }
 
 console.log(shout_backwards("Hello World"));
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -1,59 +1,59 @@
- // The data store:
- var usersData = [{
-     firstName: "SpongeBob",
-     lastName: "SquarePants",
-     email: "spongebob@crustycrab.com",
-     id: 102
- }, {
-     firstName: "Patrick",
-     lastName: "Star",
-     email: "patric.star@gmail.com",
-     id: 103
- }, {
-     firstName: "Squidward",
-     lastName: "Tentacles",
-     email: "awesomesquidward@yahoo.com",
-     id: 104
- }]
-     // Our object with the chainable methods
-  class UserController {
+// The data store:
+var usersData = [{
+    firstName: "SpongeBob",
+    lastName: "SquarePants",
+    email: "spongebob@crustycrab.com",
+    id: 102
+}, {
+    firstName: "Patrick",
+    lastName: "Star",
+    email: "patric.star@gmail.com",
+    id: 103
+}, {
+    firstName: "Squidward",
+    lastName: "Tentacles",
+    email: "awesomesquidward@yahoo.com",
+    id: 104
+}]
+    // Our object with the chainable methods
+ class UserController {
 
-    findUser(email){
-      for (let i = 0; i < usersData.length; i++){
-        if( usersData[i]["email"] === email ){
-          this.index = i;
-          break;
-        } else {
+   findUser(email){
+     for (let i = 0; i < usersData.length; i++){
+       if( usersData[i]["email"] === email ){
+         this.index = i;
+         break;
+       } else {
 
-        }
-      }
-      return this
-    }
+       }
+     }
+     return this
+   }
 
-    formatName(){
-      this.name = "Member name: "+ usersData[this.index]["firstName"] +" "+usersData[this.index]["lastName"]
-      return this
-    }
+   formatName(){
+     this.name = "Member name: "+ usersData[this.index]["firstName"] +" "+usersData[this.index]["lastName"]
+     return this
+   }
 
-    formatData(){
-      this.id= "ID: "+ usersData[this.index]["id"]
-      this.email= "Email: "+ usersData[this.index]["email"]
-      return this
-    }
+   formatData(){
+     this.id= "ID: "+ usersData[this.index]["id"]
+     this.email= "Email: "+ usersData[this.index]["email"]
+     return this
+   }
 
-    displayUser(){
-      console.log(this.name)
-      console.log(this.id)
-      console.log(this.email)
-    }
-  };
+   displayUser(){
+     console.log(this.name)
+     console.log(this.id)
+     console.log(this.email)
+   }
+ };
 
- // Driver code
- let userController = new UserController()
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+// Driver code
+let userController = new UserController()
+userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
 
 
- // result
- // Member name: Squidward Tentacles
- // ID: 104
- // Email: awesomesquidward@yahoo.com
+// result
+// Member name: Squidward Tentacles
+// ID: 104
+// Email: awesomesquidward@yahoo.com
