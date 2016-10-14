4698127cccaf7f0a8a5d6eacfde9be41398966e9
Alessandro C <alessandro.christopher@gmail.com>
2016-10-14 10:06:00 +0700

    Submitted

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -22,17 +22,47 @@
 
  // Our object with the chainable methods using class in ES6
  class UserController {
-  
+   constructor(){
+     this.usersData = usersData
+     this.firstName =  ""
+     this.lastName = ""
+     this.email = ""
+     this.id = 0
+     this.name = ""
+     this.data = ""
+   }
+
    titleCaseName(str) {
-    
-    
+
+
+   }
+   findUser(user){
+     for (var i = 0; i < usersData.length; i++) {
+       if(usersData[i].email == user){
+         this.firstName = usersData[i].firstName
+         this.lastName = usersData[i].lastName
+         this.email = usersData[i].email
+         this.id = usersData[i].id
+       }
+     }
+     return this
+   }
+   formatName(){
+     this.name = `Member name: ${this.firstName} ${this.lastName}\n`;
+     return this
+   }
+   formatData(){
+     this.data = `ID: ${this.id}\nEmail: ${this.email}`;
+     return this
+   }
+   displayUser(){
+     console.log(this.name + this.data);
    }
  }
 
  // Driver code
  let userController = new UserController;
  userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
-
  // result
  // Member name: Squidward Tentacles
  // ID: 104
